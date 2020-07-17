31b711dca0731e03163c07395b0e3d202acf8ce0
Aaron Fu Lei <aaron.fu.lei@gmail.com>
2020-07-17 08:44:46 +0800

    b

--- 5-pointes-and-arrays/5-09.c
+++ 5-pointes-and-arrays/5-09.c
@@ -1,6 +1,8 @@
 /* Exercise 5-9. Rewrite the routines day_of_year and month_day with pointers
    instead of indexing. */
 
+#include <stdio.h>
+
 static char daytab[2][13] = {
     {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
@@ -8,7 +10,7 @@ static char daytab[2][13] = {
 
 int day_of_year(int year, int month, int day)
 {
-    int i, leap, **ptab = daytab;
+    int i, leap;
 
     leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     for (i = 0; i < month; i++)
@@ -20,11 +22,21 @@ void month_day(int year, int yearday, int *pyear, int *pmonth, int *pday)
 {
     int i, leap, day;
 
-    leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     *pyear = year;
-    *pmonth = leap ? *daytab : *(daytab + 1);
-    for (day = 0; day < yearday; ) {
-        day += daytab
+    leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+    char *p = (leap ? *(daytab + 1) : *daytab) + 1;
+    for (*pmonth = 1; yearday > *p; ++p) {
+        yearday -= *p;
+        ++*pmonth;
     }
+    *pday = yearday;
 }
 
+int main()
+{
+    int year, month, day;
+    month_day(2020, 100, &year, &month, &day);
+    printf("year = %d, month = %d, day = %d\n", year, month, day);
+    printf("day of year = %d\n", day_of_year(year, month, day));
+    return 0;
+}
