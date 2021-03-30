e7a25b3ec07c66ad0fc3d49cc7de74a75ef05465
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2021-03-30 08:46:51 +0800

    1

---
+++ 0-meat/falloc.c
@@ -0,0 +1,52 @@
+#include <stddef.h>
+
+typedef double Align;
+
+union header
+{
+    struct
+    {
+        union header * ptr;
+        unsigned       size;
+    } s;
+    Align x;
+};
+
+typedef union header Header;
+
+static Header   base;
+static Header * freep = NULL;
+
+void * malloc(unsigned nbytes)
+{
+    Header * p, * prevp;
+    Header * morecore(unsigned);
+    unsigned nunits;
+    nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1; /* round up */
+    if ((prevp = freep) == NULL) {
+        base.s.ptr = freep = prevp = &base;
+        base.s.size = 0;
+    }
+    for (p = prevp->s.ptr; ; prevp = p, p = p->s.ptr) {
+        if (p->s.size >= nunits) {
+            if (p->s.size == nunits) {
+                prevp->s.ptr = p->s.ptr;
+            }
+            else {
+                unsigned ndiff = p->s.size - nunits;
+                p += ndiff;
+                p->s.size = nunits;
+                // p->s.size -= nunits;
+                // p += p->s.size;
+                // p->s.size = nunits;
+            }
+            freep = prevp;
+            return (void *)(p + 1);
+        }
+        if (p == freep)
+            if ((p = morecore(nunits)) == NULL)
+                return NULL;
+    }
+}
+
+int main() {}
