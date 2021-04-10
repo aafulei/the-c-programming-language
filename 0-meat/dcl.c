2d7ee9d3754953f9ddd399ef758aa9b7462af216
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2021-04-10 21:34:29 +0800

    dcl

--- 0-meat/dcl.c
+++ 0-meat/dcl.c
@@ -1,60 +1,109 @@
-#include <stdio.h>
+// gcc dcl.c getch.c
+
 #include <ctype.h>
+#include <stdio.h>
 #include <string.h>
 
-char g_name[100];
-char g_token[100];
-char g_out[1000];
-int g_last_token_type;
+#define TOKEN_SIZE 100
+#define DERIVED_SIZE 1000
+
+void dcl(void);
+void ddcl(void);
+int  get_token_type(void);
+int  getch();
+int  ungetch(char);
+
+char g_name[TOKEN_SIZE];
+char g_primitive[TOKEN_SIZE];
+char g_derived[DERIVED_SIZE];
+char g_last_token[TOKEN_SIZE];
+int  g_last_token_type;
+enum { NAME, PARENTHESES, BRACKETS };
 
 void dcl(void)
 {
-    int stars = 0;
-    while (1) {
-        char t = get_token();
-        if (t == '*')
-            ++stars;
-        else
-            break;
-    }
+    int num_stars = 0;
+    while (get_token_type() == '*')
+        ++num_stars;
     ddcl();
-    while (stars-- > 0)
-        strcat(g_out, " pointer to");
+    while (num_stars-- > 0)
+        strcat(g_derived, " pointer to");
 }
 
 void ddcl(void)
 {
-    int type;
     if (g_last_token_type == '(') {
         dcl();
         if (g_last_token_type != ')')
-            printf("missing )!\n");
+            printf("ddcl(): [error] missing )\n");
     }
-    else if (g_last_token_type == g_name) {
-        strcpy(g_name, g_token);
+    else if (g_last_token_type == NAME) {
+        strcpy(g_name, g_last_token);
     }
-    else
-        printf("error: expect name or (dcl)\n");
+    else {
+        printf("ddcl(): [error] expect name or (dcl)\n");
+    }
     while (1) {
-        type = get_token();
-        if (type == PARENS) {
-            strcat(g_out, " function returning");
+        int token_type = get_token_type();
+        if (token_type == PARENTHESES) {
+            strcat(g_derived, " function returning");
         }
-        else if (type == BRACKETS) {
-            strcat(g_out, " array");
-            strcat(g_out, g_token);
-            strcat(g_out, " of");
+        else if (token_type == BRACKETS) {
+            strcat(g_derived, " array");
+            strcat(g_derived, g_last_token);
+            strcat(g_derived, " of");
         }
         else
             break;
     }
 }
 
+int get_token_type(void)
+{
+    char   c;
+    char * p = g_last_token;
+    do
+        c = getch();
+    while (c == ' ' || c == '\t');
+    if (c == '(') {
+        if ((c = getch()) == ')') {
+            strcpy(g_last_token, "()");
+            g_last_token_type = PARENTHESES;
+        }
+        else {
+            ungetch(c);
+            g_last_token_type = '(';
+        }
+    }
+    else if (c == '[') {
+        for (*p++ = c; (*p++ = getch()) != ']'; )
+            ;
+        *p = '\0';
+        g_last_token_type = BRACKETS;
+    }
+    else if (isalpha(c)) {
+        for (*p++ = c; isalnum(c = getch()); *p++ = c)
+            ;
+        *p = '\0';
+        ungetch(c);
+        g_last_token_type = NAME;
+    }
+    else {
+        g_last_token_type = c;
+    }
+    return g_last_token_type;
+}
 
 int main()
 {
-    // while (get_token() != EOF) {
-
-    // }
+    while (get_token_type() != EOF) {
+        strcpy(g_primitive, g_last_token);
+        g_derived[0] = '\0';
+        dcl();
+        if (g_last_token_type != '\n') {
+            printf("main(): [error] syntax error\n");
+        }
+        printf("%s: %s %s\n", g_name, g_derived, g_primitive);
+    }
     return 0;
 }
---
+++ 0-meat/getch.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define BUFFER_SIZE 100
+
+char   g_buffer[BUFFER_SIZE];
+char * g_free = g_buffer;       // always points to the slot to put back a char
+
+// return char on success, EOF on failure
+int getch()
+{
+    if (g_free == g_buffer)
+        return getchar();
+    else
+        return *--g_free;
+}
+
+// return c on success, EOF on failure
+int ungetch(char c)
+{
+    if (g_free < g_buffer + BUFFER_SIZE)
+        return *g_free++ = c;
+    else
+        return EOF;
+}
