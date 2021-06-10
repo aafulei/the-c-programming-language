913fa483ff46919396be98c9f1a3ee8a1da64f7e
Aaron Fu Lei <aaron.fu@alumni.ust.hk>
2021-06-10 23:42:40 +0800

    Update 8-06.c

--- 8-the-unix-system-interface/8-06.c
+++ 8-the-unix-system-interface/8-06.c
@@ -1,3 +1,28 @@
 /* Exercise 8-6. The standard library function calloc(n,size) returns a pointer
  * to n objects of size size, with the storage initialized to zero. Write
  * calloc, by calling malloc or by modifying it. */
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void *calloc_(size_t n, size_t size)
+{
+  void *ret = malloc(n * size);
+  memset(ret, 0, n * size);
+  return ret;
+}
+
+int main()
+{
+  int *a = calloc_(10, sizeof(int));
+  for (int i = 0; i != 10; ++i)
+    printf("%d: %d\n", i, a[i]);
+  char *s = calloc_(10, sizeof(char));
+  s[0] = 'a';
+  s[1] = 'b';
+  s[2] = 'c';
+  printf("%s\n", s);
+  return 0;
+}
